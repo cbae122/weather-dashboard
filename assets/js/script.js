b959623d81e14ba190649e7876a241525d161bcd
cbae <xchris.bae@gmail.com>
2023-04-02 14:02:18 -0700

    working on localstorage for cities searched

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,13 @@
-// GIVEN a weather dashboard with form inputs
-// WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the the wind speed
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
-
 var today = dayjs();
 var todaysDate = (today.format('MM/DD/YYYY'));
-var city = '';
-var citySearch = $('#city-name');
+var citySearched = [];
+var citySearchEl = $('#searchedCity');
+var searchedCityName;
 var submitButtonEl = $('submit-button');
 var formEl = document.querySelector('#city-search-form');
 var formInputEl = document.querySelector('#city-name');
 var apiKey = 'b63352d7a434d5e352882d0272d386e4';
+
 
 function handleFormSubmit(event) {
     event.preventDefault();
@@ -36,6 +28,10 @@ function firstApi(query) {
         })
         .then(function (data) {
             console.log(data);
+            if (citySearched.includes(data.name) === false) {
+                citySearched.push(data.name);
+                localStorage.setItem('city', JSON.stringify(citySearched));
+            }
 
             var weatherSymbol = data.weather[0].icon;
             var symbolUrl = `https://openweathermap.org/img/wn/` + weatherSymbol + `@2x.png`;
@@ -56,6 +52,7 @@ function firstApi(query) {
             var lon = data.coord.lon;
             console.log('lat: ' + lat + ' lon:' + lon);
             secondApi(lat, lon);
+
         })
 };
 
@@ -118,5 +115,9 @@ function secondApi(lat, lon) {
             };
         });
 };
+
+// function displaySearchedCities () {
+//     if (localStorage.getItem('city'))
+// }
 
 formEl.addEventListener('submit', handleFormSubmit);
